3908aa5bfec1b526a8d3a231bb4fc600504316b1
Alex Szefezuk <alexszefezuk@hotmail.com>
2024-09-28 10:53:08 -0300

    feat: add items form

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,23 @@
+const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { number, item } = e.target
+    console.log(number.value, item.value)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="number" type="Number" />
+      <input name="item" type="text" />
+      <button type="submit">Adicionar</button>
+    </form>
+  )
+}
+
 const App = () => (
-  <h1 className="text-3xl font-bold underline">Boilerplate Vite + React</h1>
+  <main className="max-w-7xl h-screen mx-auto">
+    <Form />
+  </main>
 )
 
 export { App }
