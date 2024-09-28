2e1023320c49b2164ac53fb04111f27e781c93a0
Alex Szefezuk <alexszefezuk@hotmail.com>
2024-09-28 11:10:12 -0300

    style: form items

--- src/app.jsx
+++ src/app.jsx
@@ -6,16 +6,40 @@ const Form = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name="number" type="Number" />
-      <input name="item" type="text" />
-      <button type="submit">Adicionar</button>
-    </form>
+    <div className="w-full bg-[#4B527E]">
+      <form
+        className="flex justify-center items-center gap-3 mx-auto max-w-7xl text-white h-28"
+        onSubmit={handleSubmit}
+      >
+        <p className="text-2xl text-center font-semibold">
+          O que você precisa guardar?
+        </p>
+        <input
+          className="bg-[#7D81AE] py-3 px-2 w-20 rounded-lg outline-none text-center text-lg placeholder-gray-200"
+          name="number"
+          type="Number"
+          min={1}
+          placeholder="2"
+        />
+        <input
+          className="bg-[#7D81AE] py-3 px-2 w-52 rounded-lg outline-none text-left text-lg placeholder-gray-200"
+          name="item"
+          type="text"
+          placeholder="Nome do item"
+        />
+        <button
+          className="py-3 px-2 w-36 bg-[#FF6086] rounded-lg hover:bg-[#fc446f] text-lg font-bold"
+          type="submit"
+        >
+          Adicionar
+        </button>
+      </form>
+    </div>
   )
 }
 
 const App = () => (
-  <main className="max-w-7xl h-screen mx-auto">
+  <main>
     <Form />
   </main>
 )
