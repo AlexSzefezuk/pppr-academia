27f92d8fb234691d06d463338c6cc10d8dfff936
Alex Szefezuk <alexszefezuk@hotmail.com>
2024-09-28 11:43:03 -0300

    feat: add items list

--- src/app.jsx
+++ src/app.jsx
@@ -16,7 +16,7 @@ const Form = ({ onItemsChange }) => {
   return (
     <div className="w-full bg-[#4B527E]">
       <form
-        className="flex justify-center items-center gap-3 mx-auto max-w-7xl text-white h-28"
+        className="flex justify-center items-center gap-3 mx-auto max-w-7xl text-white py-7"
         onSubmit={handleSubmit}
       >
         <p className="text-2xl text-center font-semibold">
@@ -46,6 +46,26 @@ const Form = ({ onItemsChange }) => {
   )
 }
 
+const ItemsList = ({ items }) => {
+  return (
+    <div className="w-full h-96 bg-[#E5C3A7]">
+      <div className="flex justify-center items-center gap-12 flex-wrap mx-auto max-w-7xl text-white py-9">
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className="flex gap-2 text-xl text-[#4B527E] items-center font-medium"
+          >
+            <input type="checkbox" className="size-5" />
+            <p>{item.number}</p>
+            <p>{item.item}</p>
+            <span>❌</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
 const App = () => {
   const [items, setItems] = useState([])
   const handleItemsAdd = (newItem) => setItems((i) => [...i, newItem])
@@ -53,6 +73,7 @@ const App = () => {
   return (
     <main>
       <Form onItemsChange={handleItemsAdd} />
+      <ItemsList items={items} />
     </main>
   )
 }
