990ad40815a30c4d42b933a8f12f98f17e6f2d68
Alex Szefezuk <alexszefezuk@hotmail.com>
2024-09-28 21:10:26 -0300

    feat: add sorter

--- src/app.jsx
+++ src/app.jsx
@@ -16,10 +16,12 @@ const Form = ({ onItemsChange }) => {
     const { number, item } = e.target
 
     onItemsChange({
+      createdAt: new Date(),
       id: crypto.randomUUID(),
       number: number.value,
       item: item.value,
       saved: false,
+      show: true,
     })
   }
 
@@ -58,40 +60,96 @@ const Form = ({ onItemsChange }) => {
   )
 }
 
-const ItemsList = ({ items, onSaveItem, onItemRemove, onClearList }) => {
+const ItemsList = ({
+  items,
+  onSaveItem,
+  onItemRemove,
+  onClearList,
+  onReOrder,
+}) => {
+  const handleReOrderItems = (e) => {
+    const mode = e.target.value
+    const newArray = items.map((item) => ({ ...item, show: true }))
+
+    switch (mode) {
+      case "recent":
+        onReOrder(
+          newArray.sort(
+            (itemA, itemB) =>
+              new Date(itemA.createdAt) - new Date(itemB.createdAt),
+          ),
+        )
+
+        break
+
+      case "saved":
+        onReOrder(
+          newArray.map((item) =>
+            !item.saved ? { ...item, show: false } : item,
+          ),
+        )
+        break
+
+      case "notSaved":
+        onReOrder(
+          newArray.map((item) =>
+            item.saved ? { ...item, show: false } : item,
+          ),
+        )
+        break
+
+      case "alphabetical":
+        onReOrder(
+          newArray.sort((itemA, itemB) => itemA.item.localeCompare(itemB.item)),
+        )
+        break
+    }
+  }
+
   return (
     <div className="w-full flex-1 flex items-center flex-col justify-between bg-[#E5C3A7]">
       <div className="flex justify-center items-center gap-12 flex-wrap mx-auto max-w-7xl text-white py-9">
-        {items.map((item) => (
-          <div
-            key={item.id}
-            className="flex gap-2 text-xl text-[#4B527E] items-center font-medium "
-          >
-            <input
-              type="checkbox"
-              className="size-5 hover: cursor-pointer"
-              onClick={() => onSaveItem(item.id)}
-            />
-            <p className={item.saved && "font-normal text-[#5f68a0]"}>
-              {item.number}
-            </p>
-            <p
-              className={
-                item.saved && "line-through font-normal text-[#5f68a0]"
-              }
+        {items.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className={`flex gap-6 text-xl text-[#4B527E] items-center font-medium ${
+                !item.show && "hidden"
+              }`}
             >
-              {item.item}
-            </p>
-            <span
-              className="hover: cursor-pointer"
-              onClick={() => onItemRemove(item.id)}
-            >
-              ❌
-            </span>
-          </div>
-        ))}
+              <input
+                type="checkbox"
+                className="size-5 hover: cursor-pointer"
+                onClick={() => onSaveItem(item.id)}
+              />
+              <p
+                className={
+                  item.saved && "font-normal line-through text-[#5f68a0]"
+                }
+              >
+                {item.number} {item.item}
+              </p>
+              <button
+                className="hover: cursor-pointer"
+                onClick={() => onItemRemove(item.id)}
+              >
+                ❌
+              </button>
+            </div>
+          )
+        })}
       </div>
       <div>
+        <select
+          className="bg-[#7D81AE] text-white py-3 mb-6 px-2 w-64 rounded-lg text-lg font-bold mr-4"
+          onChange={handleReOrderItems}
+        >
+          <option value="recent">Ordenar por mais recente</option>
+          <option value="alphabetical">Ordem alfabética</option>
+          <option value="saved">Guardados</option>
+          <option value="notSaved">Não guardados</option>
+        </select>
+
         <button
           className="py-3 mb-6 text-white px-2 w-36 bg-[#FF854A] rounded-lg hover:bg-[#ff7f3e] text-lg font-bold"
           onClick={onClearList}
@@ -139,6 +197,8 @@ const App = () => {
 
   const handleClearItems = () => setItems([])
 
+  const handleOrderItems = (newList) => setItems(newList)
+
   return (
     <main className="flex flex-col h-dvh">
       <Header />
@@ -148,6 +208,7 @@ const App = () => {
         onSaveItem={handleSaveItem}
         onItemRemove={handleRemoveItem}
         onClearList={handleClearItems}
+        onReOrder={handleOrderItems}
       />
       <Footer items={items} />
     </main>
